7396586a033d49d09e90ac26e589e8b4eb5f28e2
ashtyn3 <ashtyn@gitmono.com>
2020-10-20 10:18:46 -0400

    Added arg parser and added -auth flag

--- cmd/nospin.go
+++ cmd/nospin.go
@@ -2,10 +2,35 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	args "nospin/arg-parser"
+	"nospin/config"
 	"nospin/user"
+	"os"
+	us "os/user"
 )
 
 func main() {
-	// user.UserMake("ashtyn@gmail.com")
-	fmt.Println(user.Get("ashtyn@gmail.com"))
+	usr, err := us.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	home := usr.HomeDir
+	a := os.Args[1:]
+	Args := args.ArgParser(a)
+	for _, v := range Args {
+		if v.Flag == "-auth" {
+			_, err := ioutil.ReadDir(home + "/nospin")
+			if os.IsNotExist(err) == false {
+				fmt.Println("You already have an active nospin user on this device.")
+			} else {
+				u := user.Make(v.Param)
+				config.New(u)
+				fmt.Println("Created user:\n" + "Name: " + u.Name + "\nPublic Token: " + u.PubTok)
+			}
+		}
+
+	}
+
 }
