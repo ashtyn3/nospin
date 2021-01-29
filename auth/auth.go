c2a6fef5c4a1df3c21c117a8063bc7b11a5bfdd6
Ashtyn <ashtyn3>
2021-01-29 17:22:03 -0500

    made built in source changing

--- auth/auth.go
+++ auth/auth.go
@@ -4,23 +4,28 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	"qoute/config"
 )
 
 type Credentials struct {
 	Url string
 	Pd  string
 }
-
 func Auth() Credentials {
 	godotenv.Load("../.env")
+	url := config.Get("url")	
+	if url == "" {
+		// @zap var: url
+		var Url string = os.Getenv("url")
 
-	// @zap var: url
-	var Url string = os.Getenv("url")
-
-	// @zap var: pd
-	var Pd string = os.Getenv("pd")
-
-	return Credentials{Url: Url, Pd: Pd}
+		// @zap var: pd
+		var Pd string = os.Getenv("pd")
+		return Credentials{Url: Url, Pd: Pd}
+	} else if url != "" {
+		url := config.Get("url")
+		pd := config.Get("pd")
+		return Credentials{Url: url, Pd: pd}
+	}
 }
 
 var _ string = os.Getenv("url")
